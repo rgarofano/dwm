f98fb9b1f6a5a9ab781217c5fae6842814e830dc
Ryan Garofano <ryan@ryangarofano.ca>
2025-06-17 21:34:51 -0700

    Add bookmarks hotkey and volume control

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
@@ -96,6 +98,9 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+        { MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("$HOME/scripts/bookmarks.sh") },
+        { 0,         XF86XK_AudioLowerVolume,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -1%") },
+        { 0,         XF86XK_AudioRaiseVolume,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +1%") },
 };
 
 /* button definitions */

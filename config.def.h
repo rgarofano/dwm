b50a2597f263dac464c445be33a50ba703964c69
Ryan Garofano <ryan@ryangarofano.ca>
2025-06-28 11:09:04 -0700

    Change font

--- config.def.h
+++ config.def.h
@@ -2,14 +2,17 @@
 
 #include <X11/XF86keysym.h>
 
+#define FONT "MesloLGL Nerd Font"
+#define SYMBOLS "JoyPixels"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { FONT":size=11", SYMBOLS":size=10" };
+static const char dmenufont[]       = FONT":size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";

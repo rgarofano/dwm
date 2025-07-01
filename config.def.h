79f9db3bd06f900621da5bb3fc7fde1b03c64085
Ryan Garofano <ryan@ryangarofano.ca>
2025-06-30 20:12:29 -0700

    Make kitty swallow

--- config.def.h
+++ config.def.h
@@ -9,6 +9,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { FONT":size=11", SYMBOLS":size=10" };
@@ -32,10 +33,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance    title       tags mask     isfloating   monitor */
-	{ "thunderbird", NULL,       NULL,       1 << 0,            0,           -1 },
-	{ "Signal",      NULL,       NULL,       1 << 0,            0,           -1 },
-	{ "librewolf",   NULL,       NULL,       1 << 1,            0,           -1 },
+	/* class        instance    title       tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "thunderbird", NULL,       NULL,       1 << 0,        0,       0,           -1,       -1 },
+	{ "Signal",      NULL,       NULL,       1 << 0,        0,       0,           -1,       -1 },
+	{ "librewolf",   NULL,       NULL,       1 << 1,        0,       0,           -1,       -1 },
+	{ "kitty",       NULL,       NULL,            0,        0,       1,            0,       -1 },
+	{ NULL,          NULL, "Event Tester",        0,        0,       0,            1,       -1 }, /* xev */
 };
 
 /* layout(s) */
